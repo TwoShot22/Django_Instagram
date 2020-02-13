568d2e99ed2d51212559f625acc9798f1f8f7012
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-13 16:56:03 +0900

    Create Page/Model

--- page/models.py
+++ page/models.py
@@ -1,3 +1,8 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 20)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    content = models.TextField()
