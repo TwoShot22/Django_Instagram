b1d5502ad8a9e982b70f7aa871dec3358544a077
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-13 17:45:48 +0900

    Detail.html Added

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ import page.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', page.views.home, name="home"),
+    path('detail/<int:post_id>', page.views.detail, name="detail")
 ]
