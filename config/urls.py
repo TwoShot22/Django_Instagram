78f023d8f7f33cccdd4514723973a5bd9b35a0aa
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-20 18:50:55 +0900

    Add hello.js in page/js

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
 import page.views
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', page.views.home, name="home"),
     path('detail/<int:post_id>', page.views.detail, name="detail"),
     path('new/', page.views.new, name="new"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
