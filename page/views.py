5c35b6a3b4636d590365958d5482fdeedd62c3d6
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-20 18:35:30 +0900

    Add Static Files (CSS)

--- page/views.py
+++ page/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import Post
 
 def home(request):
@@ -10,4 +11,15 @@ def detail(request, post_id):
     return render(request, 'detail.html', {'post':post})
 
 def new(request):
-    return render(request, 'new.html')
+    # 글 작성
+    if request.method == 'POST':
+        post = Post()
+        post.author = request.user
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.pub_date = timezone.datetime.now()
+        post.save()
+
+        return redirect('/detail/'+str(post.id))
+    else:
+        return render(request, 'new.html')
